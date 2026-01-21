147f938ca031d8e2907486f4e8b0a1c0ee70d599
Afan Khan <mrkhanads@gmail.com>
2026-01-21 19:37:52 +0530

    Created routes using React Router (Pages router)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+// In react-router-dom, the <Routes> component acts as a container for all route definitions, while the <Route> component defines a specific mapping between a URL path and the component (or "element") that should be rendered when that path matches. 
+import { Routes, Route } from 'react-router-dom'
+
+// Importing the pages we created in the /pages directory for setting up the routes.
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
 
 const App = () => {
   return (
-    <div className='bg-red-500'>App</div>
+      <div>
+          {/* This is the main container for all the routes. */}
+          <Routes>
+              {/* Setting up individual routes for the pages. */}
+              <Route path='/' element={<Home />} />
+
+              {/* <Route path='(website URL, e.g. domain.com/result)' element={<Component />}/> is the syntax for setting up a route. */}
+              <Route path='/result' element={<Result />} />
+              <Route path='/buy' element={<BuyCredit />} />
+          </Routes>
+    </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { BrowserRouter } from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // We replaced StrictMode with BrowserRouter to enable React Router.
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
